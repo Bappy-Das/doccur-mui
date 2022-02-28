f918d3020b35b10bb0d1973906f0aced300a8be3
Bappy-Das <bappydas.bd51@gmail.com>
2022-03-01 04:40:19 +0600

    doctor patient added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Headers from '../Shared/Headers/Headers';
 // import HeadersDropdown from '../Shared/Headers/HeadersDropdown';
 
 import Banner from './Banner/Banner';
+import LookingFor from './LookingFor/LookingFor';
 import MainService from './MainService/MainService';
 import Specialities from './Specialities/Specialities';
 
@@ -15,6 +16,7 @@ const Home = () => {
             <Banner></Banner>
             <MainService></MainService>
             <Specialities></Specialities>
+            <LookingFor></LookingFor>
         </div>
     );
 };
